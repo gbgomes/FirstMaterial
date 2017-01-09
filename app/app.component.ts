04cef8dc29870847ad574f2379510ed8b4bd59ac
Guilherme Gomes <guilherme,gomes@gmail.com>
2017-01-09 20:04:03 -0200

    lista de Herois, e classe heroi

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { Heroi } from 'app/heroi';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>{{title}}</h1><h2>{{hero}} details!</h2>`,
+  template: `<h1>{{title}}</h1>
+  <h2>Meu heroi preferido é: {{meuHeroi.nome}}</h2>
+  <ul>
+  <li *ngFor="let heroi of herois">
+  {{heroi.nome}}
+  </li>
+  </ul>
+  <p *ngIf="herois.length > 3"> Muitos Herois!!!!</p>`
 })
 export class AppComponent  { 
-	title = 'Tour of Heroes';
-  	hero = 'Windstorm';
+	title = 'Lista de Herois';
+
+  	herois = [ new Heroi(1,'SuperHomem'), new Heroi(2,'Batman'), new Heroi(3,'Home de Ferro'), new Heroi(4,'Mulher Maravilha')];
+
+  	meuHeroi = this.herois[0];
+
 }
---
+++ app/heroi.ts
@@ -0,0 +1,9 @@
+export class Heroi 
+{
+	constructor
+	(
+		public id: number,
+		public nome: string
+	) 
+	{ }
+}
