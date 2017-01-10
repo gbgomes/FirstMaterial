2d6cc6f146e811cf875a5242ddf3315feee68c6d
Guilherme Gomes <guilherme,gomes@gmail.com>
2017-01-10 19:10:09 -0200

    tutorial

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,26 +4,16 @@ import { Hero } from './hero';
 @Component({
   selector: 'my-app',
   template: `
-
-  <h1>{{title}}</h1>
-  <ul class="heroes">
-      <li *ngFor="let hero of heroes"
-        [class.selected]="hero === selectedHero"
-        (click)="onSelect(hero)">
-        <span class="badge">{{hero.id}}</span> {{hero.name}}
-      </li>
- </ul>
-
-    <div *ngIf="selectedHero">
-      <h2>{{selectedHero.name}} details!</h2>
-      <div><label>id: </label>{{selectedHero.id}}</div>
-      <div>
-        <label>name: </label>
-        <input [(ngModel)]="selectedHero.name" placeholder="name"/>
-      </div>
-    </div>
-
-  <p *ngIf="heroes.length > 3"> Muitos Herois!!!!</p>`,
+	  <h1>{{title}}</h1>
+	  <ul class="heroes">
+	      <li *ngFor="let hero of heroes"
+	        [class.selected]="hero === selectedHero"
+	        (click)="onSelect(hero)">
+	        <span class="badge">{{hero.id}}</span> {{hero.name}}
+	      </li>
+	 </ul>
+	 <my-hero-detail [hero]="selectedHero"></my-hero-detail>
+  `,
 
   styles: [`
   .selected {
